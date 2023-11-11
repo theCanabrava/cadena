56935142299e67a5a3aa1f450e5cd132869294ae
Victor Canabrava <victor.canabrava@base2.com.br>
2023-11-10 21:59:37 -0300

    Label de data nos gráficos

--- src/screens/home/Progress/Graph.tsx
+++ src/screens/home/Progress/Graph.tsx
@@ -5,20 +5,21 @@ const Graph = () => {
 
     return (
         <View style={styles.container}>
-            <Bar value={4/7} label={4} color={Palette.deepPurple.t600}/>
-            <Bar value={6/7} label={6} color={Palette.red.t600}/>
-            <Bar value={2/7} label={2} color={Palette.green.t600}/>
-            <Bar value={7/7} label={7} color={Palette.deepPurple.t600}/>
+            <Bar value={4/7} label={4} color={Palette.deepPurple.t600} date={new Date()}/>
+            <Bar value={6/7} label={6} color={Palette.red.t600} date={new Date()}/>
+            <Bar value={2/7} label={2} color={Palette.green.t600} date={new Date()}/>
+            <Bar value={7/7} label={7} color={Palette.deepPurple.t600} date={new Date()}/>
             <Bar value={0} label={0} color={Palette.deepPurple.t100}/>
             <Bar value={0} label={0} color={Palette.deepPurple.t100}/>
             <Bar value={0} label={0} color={Palette.deepPurple.t100}/>
             <Bar value={0} label={0} color={Palette.deepPurple.t100}/>
+            <View style={styles.underline}/>
         </View>
     )
 
 }
 
-const Bar = ({value, label, color}: {value: number, label: number, color: string}) => {
+const Bar = ({value, label, color, date}: {value: number, label: number, color: string, date?: Date}) => {
 
     const labelStyle = { ...styles.barLabel };
     if(label === 0) labelStyle.color === Palette.grey.t500;
@@ -28,6 +29,8 @@ const Bar = ({value, label, color}: {value: number, label: number, color: string
         backgroundColor: color,
         height: value > 0 ? value * 140 : 9
     };
+
+    const dateString = date ? `${date.getDate()}/${date.getMonth()+1}` : '';
     
 
     return (
@@ -36,6 +39,9 @@ const Bar = ({value, label, color}: {value: number, label: number, color: string
                 {label === 0 ? '-' : label}
             </Text>
             <View style={barStyle}/>
+            <Text style={styles.dateLabel}>
+                {dateString}
+            </Text>
         </View>
     )
 }
@@ -48,9 +54,7 @@ const styles = StyleSheet.create(
             flexDirection: 'row',
             justifyContent: 'space-around',
             alignItems: 'flex-end',
-            marginBottom: 24,
-            borderBottomWidth: 2,
-            borderBottomColor: Palette.deepPurple.t300
+            marginBottom: 24
         },
 
         barLabel: {
@@ -70,8 +74,29 @@ const styles = StyleSheet.create(
             backgroundColor: Palette.deepPurple.t600,
             borderTopLeftRadius: 4,
             borderTopRightRadius: 4
-        }
+        },
 
+        dateLabel: {
+            fontSize: 10,
+            lineHeight: 18,
+            fontFamily: 'Roboto-Regular',
+            color: Palette.grey.t900,
+            transform: [{rotate: '-45deg'}],
+            height: 18,
+            width: 27,
+            position: 'relative',
+            right: 12,
+            marginTop: 5
+        },
+
+        underline: {
+            position: 'absolute',
+            bottom: 22,
+            left: 0,
+            right: 0,
+            height: 2,
+            backgroundColor: Palette.deepPurple.t300
+        }
 
     }
 )
